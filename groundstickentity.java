36c5aa9166faac7c96d15983e548fd2b7f14aa38
vyyvvuvvuvuvyuvvuvyuvy <157741817+vyyvvuvvuvuvyuvvuvyuvy@users.noreply.github.com>
2025-01-30 14:15:31 -0600

    Create groundstickentity.java

---
+++ groundstickentity.java
@@ -0,0 +1,26 @@
+package com.yourname.realismmod;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.world.World;
+
+public class GroundStickEntity extends EntityItem {
+    public GroundStickEntity(World world) {
+        super(world);
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+    }
+}
+
+public class GroundRockEntity extends EntityItem {
+    public GroundRockEntity(World world) {
+        super(world);
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+    }
+}
